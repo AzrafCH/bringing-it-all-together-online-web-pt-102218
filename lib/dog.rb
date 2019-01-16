61a725d5d97a4c202ceb6064e94bef772fb0c200
Azraf Chowdhury <achowdhur@ltu.edu>
2019-01-16 17:28:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,10 @@ class Dog
 attr_accessor :name, :breed
 attr_reader :id
 
-#
+  def initialize(name, breed, id = nil)
+    @name = name
+    @breed = breed
+  end
 
   def self.create_table
     sql = <<-SQL
@@ -19,7 +22,7 @@ attr_reader :id
 
   def self.drop_table
     sql = <<-SQL
-      DROP TABLE IF NOT EXISTS dogs;
+      DROP TABLE IF EXISTS dogs;
     SQL
 
     DB[:conn].execute(sql)
