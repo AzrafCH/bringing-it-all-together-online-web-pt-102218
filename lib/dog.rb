2a6347fb48460846a58b5456404ef3fb4fdd2077
Azraf Chowdhury <achowdhur@ltu.edu>
2019-01-16 17:23:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,11 +4,21 @@ attr_accesor :name, :breed
 attr_reader :id
 
   def initialize(name, breed, id = nil)
-    @name = name 
-    @breed = breed 
-    @id = id 
-  end 
+    @name = name
+    @breed = breed
+    @id = id
+  end
 
-  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
 
 end
